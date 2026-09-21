521f67a9cdf5e3b032e47f796e8a0435507dd56c
mejaz56 <mubin.ejaz56@login.cuny.edu>
2026-09-20 23:40:56 -0400

    Add files via upload

---
+++ data607_movie_ratings.sql
@@ -0,0 +1,276 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict eAti9LZzL223Ce3FC0euTSV4iaQ2bNPdao9fheFaBrH7WQeoTkoHGAt4VTxhCmO
+
+-- Dumped from database version 18.6
+-- Dumped by pg_dump version 18.6
+
+-- Started on 2026-09-20 23:39:04
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 220 (class 1259 OID 16390)
+-- Name: movies; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movies (
+    movie_id integer NOT NULL,
+    title character varying(200) NOT NULL
+);
+
+
+ALTER TABLE public.movies OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16389)
+-- Name: movies_movie_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.movies_movie_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.movies_movie_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 5033 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: movies_movie_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.movies_movie_id_seq OWNED BY public.movies.movie_id;
+
+
+--
+-- TOC entry 223 (class 1259 OID 16406)
+-- Name: ratings; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ratings (
+    rating integer,
+    movie_id integer NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT ratings_rating_check CHECK (((rating >= 1) AND (rating <= 5)))
+);
+
+
+ALTER TABLE public.ratings OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 16399)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    name character varying(100)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16398)
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 5034 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- TOC entry 4865 (class 2604 OID 16393)
+-- Name: movies movie_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN movie_id SET DEFAULT nextval('public.movies_movie_id_seq'::regclass);
+
+
+--
+-- TOC entry 4866 (class 2604 OID 16402)
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- TOC entry 5024 (class 0 OID 16390)
+-- Dependencies: 220
+-- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movies (movie_id, title) FROM stdin;
+1	The Odyssey
+2	Toy Story 5
+3	The Housemaid
+4	Michael
+5	Spider-man, Brand New Day 
+6	Anora
+\.
+
+
+--
+-- TOC entry 5027 (class 0 OID 16406)
+-- Dependencies: 223
+-- Data for Name: ratings; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.ratings (rating, movie_id, user_id) FROM stdin;
+5	1	1
+3	2	1
+4	4	1
+5	5	1
+5	1	2
+3	2	2
+5	4	2
+5	5	2
+5	1	3
+3	2	3
+5	3	3
+5	4	3
+3	5	3
+5	1	5
+3	2	5
+5	3	5
+4	4	5
+4	5	5
+5	6	1
+4	6	2
+5	6	3
+5	6	5
+\N	3	4
+\N	3	1
+\N	3	2
+1	1	4
+1	2	4
+1	4	4
+1	5	4
+1	6	4
+\.
+
+
+--
+-- TOC entry 5026 (class 0 OID 16399)
+-- Dependencies: 222
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, name) FROM stdin;
+1	Areeb Q
+2	Zainab N
+3	Nasir H
+5	Mubin E
+4	Shameer Q
+\.
+
+
+--
+-- TOC entry 5035 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: movies_movie_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.movies_movie_id_seq', 6, true);
+
+
+--
+-- TOC entry 5036 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 5, true);
+
+
+--
+-- TOC entry 4869 (class 2606 OID 16397)
+-- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pkey PRIMARY KEY (movie_id);
+
+
+--
+-- TOC entry 4873 (class 2606 OID 16413)
+-- Name: ratings ratings_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_pkey PRIMARY KEY (user_id, movie_id);
+
+
+--
+-- TOC entry 4871 (class 2606 OID 16405)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 4874 (class 2606 OID 16414)
+-- Name: ratings ratings_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(movie_id);
+
+
+--
+-- TOC entry 4875 (class 2606 OID 16419)
+-- Name: ratings ratings_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+-- Completed on 2026-09-20 23:39:05
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict eAti9LZzL223Ce3FC0euTSV4iaQ2bNPdao9fheFaBrH7WQeoTkoHGAt4VTxhCmO
+
